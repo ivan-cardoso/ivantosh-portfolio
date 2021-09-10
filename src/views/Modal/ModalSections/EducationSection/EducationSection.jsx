69e66ad745a6192dd17e55e72b4dcfb9b2e2ece6
ivan-cardoso <ivancardoso59@gmail.com>
2021-09-10 15:38:13 -0300

    Education Section

--- src/views/Modal/ModalSections/EducationSection/EducationSection.jsx
+++ src/views/Modal/ModalSections/EducationSection/EducationSection.jsx
@@ -23,8 +23,40 @@ const EducarionSection = () => {
 
                         data.map((e)=>{
                             return(
-                                <div>
-                                    <h2>{e.title}</h2>
+                                <div className={s.education__container} >
+                                    <div className={s.title__background} >
+                                        <h2>{e.title}</h2>
+                                    </div>
+                                    <h3  className={s.title__h3} >{e.place}</h3>
+                                    <h5 className={s.title__h5}>{e.duration}</h5>
+
+                                    <div className={s.modal__text__container__bg} >
+                                        {e.description && 
+                                            <ul>
+                                                {e.description.map((text)=>{
+                                                    return (
+                                                        <li>
+                                                            <p className={s.modal__text} >{text}</p>
+                                                        </li>
+                                                        )
+                                                    })}
+                                            </ul>
+                                        }
+                                    </div>
+                                    {e.technologies &&
+                                        <>
+                                        <h3 className={s.title__h3}>TECNOLOGÍAS APRENDIDAS</h3>
+                                        <ul>
+                                            {e.technologies.map((tech)=>{
+                                                return(
+                                                    <li>
+                                                        <p className={s.modal__text}>{tech}</p>
+                                                    </li>
+                                                )
+                                            })}
+                                        </ul>
+                                        </>
+                                    }
                                 </div>
                             )
                         })
